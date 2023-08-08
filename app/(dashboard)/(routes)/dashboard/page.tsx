0c37222b5e80639571a114708c283af0aa0f50c7
raza <razakhanha05@gmail.com>
2023-08-08 15:21:14 +0530

    Added Dashboard page UI

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,83 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import {
+  ArrowRight,
+  Code,
+  Image,
+  MessageSquare,
+  Music2Icon,
+  Video,
+} from "lucide-react";
 
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Music Generation",
+    icon: Music2Icon,
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generation",
+    icon: Image,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/code",
+  },
+];
 export default function DashboardPage() {
+  const router = useRouter();
   return (
     <div>
-      Dashboard page(protected)
-      <UserButton afterSignOutUrl="" />
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl  md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
